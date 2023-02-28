69fa336dcbae4af15baa1008af7cad336a46b486
austin c <ac6404@gmail.com>
2023-02-28 08:47:19 -0600

    clean up javascript and fix off by 1

--- front-end/src/components/MultipleChoice.vue
+++ front-end/src/components/MultipleChoice.vue
@@ -1,24 +1,29 @@
 <script setup>
-  import data from '../data.json';
+  import { ref } from 'vue'
+  import data from '../data.json'
 
-  // console.log(data)
-  let currentScore = 0
-  let currentIndex = 0;
-  let englishWord  = data.words[currentIndex].english
-  let spanishWord = data.words[currentIndex].spanish
+  let quizIndex = ref(0)
+  let englishWord  = ref(data.words[quizIndex.value].english)
+  let spanishWord = ref(data.words[quizIndex.value].spanish)
   let answerOptions = []
   let quizLength = data.words.length
-  let selectedAnswer = ""
+  let selectedAnswer = ref("")
+  let message = ref("")
 
-  let gradeMsg = ""
 
   function setAnswerOptions() {
-    // add the correct answer to the answer options
-    answerOptions.push(data.words[currentIndex].spanish)
+    // reset to an empty array
+    answerOptions = []
+
+    // add the correct answer
+    answerOptions.push(data.words[quizIndex.value].spanish)
     
     // add 3 random options that are not the correct answer
-    let choice = currentIndex
+    let choice = quizIndex.value
+    
     for(let i=0; i < 3; i++){
+
+      // don't add words that are already in the array
       while(answerOptions.includes(data.words[choice].spanish)) {
         choice = Math.floor(Math.random() * data.words.length)
       }
@@ -26,50 +31,64 @@
       answerOptions.push(data.words[choice].spanish)
     }
 
-    // scramble the answer options array
+    // scramble the answer options so the correct answer isnt
+    // always in the same position
     answerOptions = answerOptions.sort((a, b) => 0.5 - Math.random());
   }
 
-  function checkAnswer(){
-    console.log(selectedAnswer)
-    console.log(data.words[currentIndex].spanish)
-    return  selectedAnswer == data.words[currentIndex].spanish
+  function isCorrectAnswer(){
+    return  selectedAnswer.value == data.words[quizIndex.value].spanish
   }
 
-  function submitAnswer(){
-    console.log("Button was clicked")
-    console.log(checkAnswer())
-    // The answer is correct
-    if(checkAnswer()){
+  function isLastQuestion() {
+    return quizIndex.value == quizLength - 1
+  }
 
-    // increment the score
-    currentScore += 1
-    console.log(currentScore)
-    // move to the next question
-    currentIndex +=1
+  function loadNextQuestion(){
 
-    gradeMsg = ""
+    quizIndex.value +=1
+
+    englishWord.value  = data.words[quizIndex.value].english
+    spanishWord.value = data.words[quizIndex.value].spanish
 
     //reset the quiz options
     setAnswerOptions()
+    selectedAnswer.value = ""
+    message.value = ""
 
-    // the answer is incorrect    
-    } else {
-      gradeMsg = "Incorrect, Try Again."
-    }
-    this.$forceUpdate()
   }
 
-  
-  setAnswerOptions()
-  console.log(answerOptions)
+  function completeQuiz() {
+    message.value = "Congratulations, you finished the quiz!"
+  }
 
+  function submitAnswer(){
+
+      if(isCorrectAnswer()){
+
+        if(isLastQuestion()) {
+          completeQuiz()
+        } else {
+          loadNextQuestion()
+        }
+   
+      } else {
+        message.value = "Incorrect, Try Again."
+      }
+
+  }
+
+  function initializeQuiz() {
+    setAnswerOptions()
+  }
+ 
+  initializeQuiz()
 
 </script>
 
 <template>
   <div class="content">
-    <div class = "progress">{{ currentIndex }}/{{ quizLength }}</div>
+    <div class = "progress">{{ quizIndex + 1 }}/{{ quizLength }}</div>
     <div class = "question">
         <span class = "prompt">How do you say </span>
         <span class = "keyword">"{{englishWord}}"</span>
@@ -81,7 +100,7 @@
         </div>
     </div>
     <button @click="submitAnswer" type ="button"> Submit </button>
-    <div class="grade-msg">{{ gradeMsg }}</div>
+    <div class="message">{{ message }}</div>
   </div>
 </template>
 
