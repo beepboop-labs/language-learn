14edb9babee99d1be9a5406c857884c6cb4208f5
austin c <ac6404@gmail.com>
2023-02-27 20:00:08 -0600

    add dynamic elements

--- front-end/src/components/MultipleChoice.vue
+++ front-end/src/components/MultipleChoice.vue
@@ -1,54 +1,62 @@
 <script setup>
-import data from '../data.json';
+  import data from '../data.json';
+  // console.log(data)
+  let currentScore = 0
+  let currentIndex = 0;
+  let englishWord  = data.words[currentIndex].english
+  let spanishWord = data.words[currentIndex].spanish
+  let answerOptions = []
+  let quizLength = data.words.length
 
-console.log(data)
+  let gradeMsg = ""
 
-let currentScore = 0
-let english  = data.words[currentIndex].english
-let spanish = data.words[currentIndex].spanish
-let currentIndex = 0;
-let answerOptions = []
+  function setAnswerOptions() {
+    // add the correct answer to the answer options
+    answerOptions.push(data.words[currentIndex].spanish)
+    
+    // choose 3 random options that are not the correct answer
+    for(let i=0; i < 3; i++){
+      let choice = currentIndex
 
-let gradeMsg = ""
+      while(choice == currentIndex) {
+        choice = Math.floor(Math.random() * data.words.length)
+      }
 
-function setAnswerOptions() {
-  // add the correct answer to the answer options
-  answerOptions.push(data.words[currentIndex].spanish)
-  
-  // choose 3 random options that are not the correct answer
-  for(let i=0; i < 3; i++){
-    let choice = currentIndex
-
-    while(choice == currentIndex) {
-      choice = Math.floor(Math.random() * data.words.length)
+      answerOptions.push(data.words[choice].spanish)
     }
 
-    answerOptions.push(data.words[choice].spanish)
+    // scramble the answer options array
+    answerOptions = answerOptions.sort((a, b) => 0.5 - Math.random());
   }
-}
 
-function checkAnswer(str){
-  return  str == data.words[currentIndex].spanish
-}
+  function checkAnswer(str){
+    return  str == data.words[currentIndex].spanish
+  }
 
-function submitAnswer(str){
+  function submitAnswer(str){
+    
+    // The answer is correct
+    if(checkAnswer(str)){
+
+    // increment the score
+    currentScore += 1
+
+    // move to the next question
+    currentIndex +=1
+
+    gradeMsg = ""
+
+    //reset the quiz options
+    setAnswerOptions()
+
+    // the answer is incorrect    
+    } else {
+      gradeMsg = "Incorrect, Try Again."
+    }
+  }
+
   
-  // The answer is correct
-  if(checkAnswer(str)){
-  // increment the score
-  currentScore += 1
-
-  // move to the next question
-  currentIndex +=1
-
-  gradeMsg = ""
-
-  //reset the quiz options
-  setAnswerOptions()    
-  } else {
-    gradeMsg = "Incorrect"
-  }
-}
+  setAnswerOptions()
 
 
 
@@ -56,13 +64,10 @@ function submitAnswer(str){
 
 <template>
   <div class="content">
+    <div class = "progress">{{ currentIndex }}/{{ quizLength }}</div>
     <div class = "question">
-        <div class = "prompt">
-            How do you say 
-        </div>
-        <div class = "keyword">
-            Yourself
-        </div>
+        <span class = "prompt">How do you say </span>
+        <span class = "keyword">"{{englishWord}}"</span>
     </div>
     <div class = "options">
         <div>
@@ -71,6 +76,7 @@ function submitAnswer(str){
         </div>
     </div>
     <button type ="button"> Submit </button>
+    <div class="grade-msg">{{ gradeMsg }}</div>
   </div>
 </template>
 
