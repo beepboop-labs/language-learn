fa0d7fd065124b5366a4f1ae47410004407c251d
austin c <ac6404@gmail.com>
2023-03-14 17:46:15 -0500

    add navbar and footer components

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -1,11 +1,21 @@
-<template>
-  <div id="app">
-    <router-view />
-  </div>
-</template>
-
 <script>
+import Navbar from '@/components/Navbar.vue'
+import Footer from '@/components/Footer.vue'
+
 export default {
   name: "App",
+  components: {
+    Navbar,
+    Footer
+  }
 };
 </script>
+
+<template>
+  <Navbar />
+  <div id="content">
+    <router-view />
+  </div>
+  <Footer />
+</template>
+
---
+++ front-end/src/components/Footer.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="footer">
+    <h3>Footer</h3>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
