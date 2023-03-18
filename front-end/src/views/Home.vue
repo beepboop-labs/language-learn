af81c7a1362f057f669a920895a3300b4ccc71a9
harjothkaur <harjothkaur1999@gmail.com>
2023-03-17 23:24:14 -0500

    modified home

--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,46 +1,32 @@
-<script setup>
-
-
-</script>
-
 <template>
-  <div class="row">
+  <div class="container">
+    <div class="row">
         <div class="col-md-12">
-            <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-
-                        <img src="assests/Images/istockphoto-1268465415-612x612.jpg" class="d-block w-100" alt="...">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="images/istockphoto-1268465415-612x612.jpg" class="d-block w-100" alt="...">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="images/istockphoto-1268465415-612x612.jpg" class="d-block w-100" alt="...">
-                    </div>
-                </div>
-                <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
-            </div>
-
+            <img src="../assets/Images/istockphoto-1268465415-612x612.jpg" class="image" alt="...">
         </div>
     </div>
-  <!-- <div id="home">
-    <h1>Homepage</h1>
-    <router-link to = "/multiple-choice">Multiple Choice Quiz</router-link>
-    <br>
-    <router-link to = "/login">Login</router-link>
-    <br>
-    <router-link to = "/register">Register</router-link>
-  </div> -->
+  </div>
 </template>
 
 <style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
 
+.image{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+.image img {
+  display: block;
+  margin: auto;
+  max-width: 100%;
+  max-height: 100%;
+}
 </style>
