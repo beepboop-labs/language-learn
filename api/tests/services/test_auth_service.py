0311e650213b37eb7f5e4da513ff3bb2b978e3e7
austin c <ac6404@gmail.com>
2023-04-12 15:22:48 -0500

    modify test config, add test.db, add demo test for db

--- api/tests/services/test_auth_service.py
+++ api/tests/services/test_auth_service.py
@@ -1,2 +1,19 @@
+from app.models.user import User
+from app import db
+
 def test_helloworld():
     assert True == True
+
+def test_create_user(client):
+    with client.application.app_context():
+        # Create a new user
+        new_user = User(username='test_user', password='bad_password')
+        db.session.add(new_user)
+        db.session.commit()
+
+        # Query the user from the database
+        user_from_db = User.query.filter_by(username='test_user').first()
+
+        # Check that the added properties match
+        assert user_from_db.username == new_user.username
+        assert user_from_db.password == new_user.password
