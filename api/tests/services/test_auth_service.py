09d67b677b96abc0f99d9f97e6d64517e2ce8ea8
austin c <ac6404@gmail.com>
2023-04-12 15:37:04 -0500

    add empty tests for login and register

--- api/tests/services/test_auth_service.py
+++ api/tests/services/test_auth_service.py
@@ -1,19 +1,60 @@
 from app.models.user import User
 from app import db
 
-def test_helloworld():
-    assert True == True
+# SAMPLE CODE FOR INTERACTING WITH DATABASE
+# def test_create_user(client):
+#     with client.application.app_context():
+#         # Create a new user
+#         new_user = User(username='test_user', password='bad_password')
+#         db.session.add(new_user)
+#         db.session.commit()
 
-def test_create_user(client):
-    with client.application.app_context():
-        # Create a new user
-        new_user = User(username='test_user', password='bad_password')
-        db.session.add(new_user)
-        db.session.commit()
+#         # Query the user from the database
+#         user_from_db = User.query.filter_by(username='test_user').first()
 
-        # Query the user from the database
-        user_from_db = User.query.filter_by(username='test_user').first()
+#         # Check that the added properties match
+#         assert user_from_db.username == new_user.username
+#         assert user_from_db.password == new_user.password
 
-        # Check that the added properties match
-        assert user_from_db.username == new_user.username
-        assert user_from_db.password == new_user.password
+class TestLogin:
+    # test user credentials
+    # *********************
+    # username: test
+    # password: password
+
+    def test_success(client):
+        assert True
+
+    def test_wrong_username(client):
+        assert True
+
+    def test_wrong_password(client):
+        assert True
+
+    def test_empty_username(client):
+        assert True
+
+    def test_empty_password(client):
+        assert True
+
+    def test_returns_user_token(client):
+        assert True
+
+class TestRegister:
+
+    def test_success(client):
+        assert True
+
+    def test_existing_username(client):
+        assert True
+
+    def test_empty_username(client):
+        assert True
+
+    def test_empty_password(client):
+        assert True
+
+    def test_returns_user_token(client):
+        assert True
+
+
