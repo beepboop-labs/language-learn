7be994aa3e0fc95add077f988e01bc92e3da9145
harjothkaur <harjothkaur1999@gmail.com>
2023-04-12 20:40:36 -0500

    New Quiz Model added True/False

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -5,6 +5,7 @@ import MultipleChoiceQuiz from "../views/MultipleChoiceQuiz.vue";
 import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
 import BlankQuiz from "../views/BlankQuiz.vue";
+import TrueFalse from "../views/TrueFalse.vue";
 
 const routes = [
     {
@@ -31,6 +32,11 @@ const routes = [
         path: "/blankquiz",
         name: "BlankQuiz",
         component: BlankQuiz,
+    },
+    {
+        path: "/truefalse",
+        name: "TrueFalse",
+        component: TrueFalse,
     }
 ];
 
