218b186841cefe181f20dde28f30ce33d10eb04d
austin c <ac6404@gmail.com>
2023-04-13 12:48:14 -0500

    fix formatting and model FK error

--- api/app/models/phrase.py
+++ api/app/models/phrase.py
@@ -3,10 +3,10 @@ from app.models.language import Language
 
 class Phrase(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    primary_language_id = db.Column(db.Integer, db.ForeignKey('language.id'), nullable=False)
-    secondary_language_id = db.Column(db.Integer, db.ForeignKey('language.id'), nullable=False)
-    primary_language = db.relationship('Language', backref='primary_phrases', lazy=True)
-    secondary_language = db.relationship('Language', backref='secondary_phrases', lazy=True)
+    primary_language_id = db.Column(db.Integer, db.ForeignKey('language.id', ondelete='CASCADE'), nullable=False)
+    secondary_language_id = db.Column(db.Integer, db.ForeignKey('language.id', ondelete='CASCADE'), nullable=False)
+    primary_language = db.relationship('Language', foreign_keys=[primary_language_id], backref='primary_phrases', lazy=True)
+    secondary_language = db.relationship('Language', foreign_keys=[secondary_language_id], backref='secondary_phrases', lazy=True)
     primary = db.Column(db.String(50), unique=True, nullable=False)
     secondary = db.Column(db.String(50), unique=True, nullable=False)
 
