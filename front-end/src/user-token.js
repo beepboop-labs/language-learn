8ab892dd251139b4bb7befc530d8075eb7646d8f
harjothkaur <harjothkaur1999@gmail.com>
2023-04-16 21:33:51 -0500

    RoadMap

--- front-end/src/user-token.js
+++ front-end/src/user-token.js
@@ -3,10 +3,12 @@ import { reactive } from 'vue'
 export let userToken = reactive({
   userId: null,
   username: null,
+  loggedin: false,
+   
   setUser(userId, username) {
     this.userId = userId,
     this.username = username
-
+    this.loggedin = true
     console.log("successfully set user in store")
   },
   getUsername(){
@@ -15,9 +17,14 @@ export let userToken = reactive({
   getUserId() {
     return this.userId;
   },
+  getLoggedIn(){
+    console.log(this.loggedin)
+    return this.loggedin;
+  },
   clearUser() {
     this.userId = null;
     this.username = null;
+    this.loggedin = false;
   }
 
 })
--- front-end/src/views/Home.vue
+++ front-end/src/views/Home.vue
@@ -1,29 +1,111 @@
-<template>
+<script>
+import { userToken } from '../user-token';
+import { ref, onMounted } from 'vue'
 
+let loggedin = ref(userToken.getLoggedIn())
+// This is a built-in lifecycle hook from Vue
+onMounted(() => {
+   loggedin.value = userToken.getLoggedIn()
+    
+  });
+
+</script>
+
+<template>
+  <div> {{loggedin}}</div>
   <div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <img src="../assets/Images/istockphoto-1268465415-612x612.jpg" class="image" alt="...">
-        </div>
-    </div>
+   <div id="login-message" v-if="!loggedin">
+    <p>Please login to view the roadmap</p>
+  </div>
   </div>
 
+
+  <div id="roadmap" v-if="loggedin">
+    <h1>SPANISH</h1>
+    <h2>Unit 1 (present, preterite, imperfect)</h2>
+    <p></p>
+    <ul>
+      <li>Multiple Choice quiz</li>
+      <li>Blank Quiz</li>
+      <li>True/False</li>
+    </ul>
+    <h2>Unit 2 (past)</h2>
+    <p></p>
+    <ul>
+      <li>Multiple Choice quiz</li>
+      <li>Blank Quiz</li>
+      <li>True/False</li>
+    </ul>
+    <h2>Unit 3 (Conditional, Future)</h2>
+    <p></p>
+    <ul>
+      <li>Multiple Choice quiz</li>
+      <li>Blank Quiz</li>
+      <li>True/False</li>
+    </ul>
+  </div>
+
+
+  <div>
+    <LoginMessage />
+    <Roadmap />
+  </div>
 </template>
 
-<style scoped>
 
+
+
+<style scoped>
+login-message {
+  background-color: #f2f2f2;
+  padding: 20px;
+  text-align: center;
+}
+
+
+#roadmap {
+  padding: 20px;
+}
+
+#roadmap h1 {
+  font-size: 24px;
+  font-weight: bold;
+}
+
+roadmap h2 {
+  font-size: 20px;
+  font-weight: bold;
+  margin-top: 30px;
+}
+
+roadmap ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+roadmap li {
+  margin-bottom: 10px;
+}
 
 .container {
+  display: flex;
   justify-content: center;
   align-items: center;
-  height: 80vh;
+  height: 100vh;
 }
 
 .image{
+  display: flex;
   justify-content: center;
   align-items: center;
-  width: 213vh;
-  height: 90vh;
+  height: 100%;
 }
 
+.image img {
+  display: block;
+  margin: auto;
+  max-width: 80%;
+  height: 50%;
+}
 </style>
