dca5a282390308d8c0a83e4623ed148c6649b2af
pbutiya <butiyapruthviraj@gmail.com>
2023-04-17 16:54:59 -0500

    Add complete quiz endpoint

--- api/app/routes/user.py
+++ api/app/routes/user.py
@@ -1,9 +1,16 @@
 from flask import request, make_response
 from flask import current_app as app
-from app.services.user_service import get_activity
+from app.services.user_service import get_activity,complete_quiz
 
 @app.route('/user/activity', methods=['POST'])
 def activity():
     request_data = request.get_json()
     response = get_activity(request_data)
+    return response
+
+
+@app.route('/user/complete-quiz', methods=['POST'])
+def complete():
+    request_data = request.get_json()
+    response = complete_quiz(request_data)
     return response
