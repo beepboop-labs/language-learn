5e118c8fa55e9af016357a18e7c8eaf6b6217760
austin c <ac6404@gmail.com>
2023-04-24 21:39:06 -0500

    add swahili translation, test cases, irregular verbs

--- api/app/utils/swahili.py
+++ api/app/utils/swahili.py
@@ -1,3 +1,27 @@
+import json
+
+f = open('data/irregular_swahili_verbs.json')
+irregular_verbs = json.load(f)
+
+subject_prefix = {
+    '1p': 'tu',
+    '2p': 'm',
+    '3p': 'wa',
+    '1s': 'ni',
+    '2s': 'u',
+    '3s': 'a'
+}
+
+subject_prefix_negative = {
+    '1p': 'ha',
+    '2p': 'ha',
+    '3p': 'ha',
+    '1s': 'si',
+    '2s': 'hu',
+    '3s': 'ha'
+}
+
+
 def parse_string(s):
     parts = s.split('+')
     if len(parts) == 4:
@@ -22,26 +46,61 @@ def conjugate_swahili(verbString):
 
     negative, subject, tense, root = parse_string(verbString)
 
-    phrase = root
+    # keep or remove 'ku' from the root
+    if root in irregular_verbs and not negative:
+        phrase = root
+    elif root in irregular_verbs and negative and tense == 'FUT':
+        phrase = root
+    else:
+        phrase = root[2:]
+        
+
 
     if tense == 'IMP':
-        return root
+        return phrase
     
     if tense == 'PAST':
         # NOTES:
-            
+    
+        if negative:
+            tense_marker = 'ku'
+            phrase = subject_prefix_negative[subject] + tense_marker + phrase
+            return phrase
+        
+        tense_marker = 'li'
+        phrase = subject_prefix[subject] + tense_marker + phrase
         return phrase
     
     if tense == 'FUT':
-
+        if negative:
+            tense_marker = 'ta'
+            phrase = subject_prefix_negative[subject] + tense_marker + phrase
+            return phrase
+        
+        tense_marker = 'ta'
+        phrase = subject_prefix[subject] + tense_marker + phrase
         return phrase
     
     if tense == 'PRES':
-
+        if negative:
+            tense_marker = 'ha'
+            if phrase[-1] == 'a':
+                phrase = phrase[:-1] + 'i'
+            phrase = subject_prefix_negative[subject] + phrase
+            return phrase
+        
+        tense_marker = 'na'
+        phrase = subject_prefix[subject] + tense_marker + phrase
         return phrase
     
     if tense == 'PERF':
-       
+        if negative:
+            tense_marker = 'ja'
+            phrase = subject_prefix_negative[subject] + tense_marker + phrase
+            return phrase
+        
+        tense_marker = 'me';
+        phrase = subject_prefix[subject] + tense_marker + phrase
         return phrase
         
     else:
