eb42cb35cc20d63ab17fe6aca3ae0dd8a6798683
austin c <ac6404@gmail.com>
2023-05-01 21:53:19 -0500

    update quiz language

--- front-end/src/views/TrueFalse.vue
+++ front-end/src/views/TrueFalse.vue
@@ -185,7 +185,7 @@
     <div id="true-false-quiz">
       <div class="progress">{{ quizIndex + 1 }}/{{ quizLength }}</div>
       <div class="question">
-        <div>Does the below translation from primary to secondary is True/False?</div>
+        <div>Does the below translation from english to {{props.secondaryLanguage}} is True/False?</div>
         <span class="prompt">{{ primaryWord }}</span>
         <br/>
         <span class="prompt">{{ secondaryWord }}</span>
